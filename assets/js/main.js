965681e6521efc092244a82a122675139a85b594
Nnnekita <kosmit.play@mail.ru>
2026-06-19 10:27:36 +0300

    fix: compile SCSS to CSS and link directly in HTML for GitHub Pages compatibility

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-import "../scss/style.scss";
+import("../scss/style.scss").catch(() => {});
 
 document.addEventListener("DOMContentLoaded", () => {
   const header = document.querySelector(".site-header");
